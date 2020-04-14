e2141edd564385aab6da0f1118dc19cf83cd5c0c
cficht <chris.ficht@gmail.com>
2020-04-13 18:18:05 -0700

    wrote and tested the date add function

--- date-adder.js
+++ date-adder.js
@@ -1,5 +1,21 @@
-const add = () => {
-
+const add = (date, diff) => {
+  inc = Number(diff.split(/(\D)/)[0])
+  type = (diff.split(/(\D)/)[1])
+  if (type === 's') {
+    return new Date(date.setSeconds(date.getSeconds() + inc));
+  } else if (type === 'm') {
+    return new Date(date.setMinutes(date.getMinutes() + inc));
+  } else if (type === 'h') {
+    return new Date(date.setHours(date.getHours() + inc));
+  } else if (type === 'd') {
+    return new Date(date.setDate(date.getDate() + inc));
+  } else if (type === 'w') {
+    return new Date(date.setDate(date.getDate() + (inc * 7)));
+  } else if (type === 'M') {
+    return new Date (date.setMonth(date.getMonth() + inc));
+  } else if (type === 'y') {
+    return new Date(date.setYear(date.getFullYear() + inc));
+  }
 };
 
 module.exports = { 
--- date-adder.test.js
+++ date-adder.test.js
@@ -2,7 +2,53 @@ const { add } = require('./date-adder');
 
 describe('testing date-adder.js', () => {
 
-  it('returns a new date with diff added to the original date', () => {
-    expect(true).toEqual(true);
+  it('returns a new date with 10 days added to the original', () => {
+    const orgDate = new Date(2000, 10, 15);
+    const diff = '10d';
+    const newDate = new Date(2000, 10, 25);
+    expect(add(orgDate, diff)).toEqual(newDate);
   });
+
+  it('returns a new date with 33 years added to the original', () => {
+    const orgDate = new Date(1960, 2, 14);
+    const diff = '33y';
+    const newDate = new Date(1993, 2, 14);
+    expect(add(orgDate, diff)).toEqual(newDate);
+  });
+
+  it('returns a new date with 9 months added to the original', () => {
+    const orgDate = new Date(2005, 5, 20);
+    const diff = '9M';
+    const newDate = new Date(2006, 2, 20);
+    expect(add(orgDate, diff)).toEqual(newDate);
+  });
+
+  it('returns a new date with 5 days added to the original', () => {
+    const orgDate = new Date(2020, 1, 20);
+    const diff = '5d';
+    const newDate = new Date(2020, 1, 25);
+    expect(add(orgDate, diff)).toEqual(newDate);
+  });
+
+  it('returns a new date with 2 weeks added to the original', () => {
+    const orgDate = new Date(1970, 5, 20);
+    const diff = '2w';
+    const newDate = new Date(1970, 6, 4);
+    expect(add(orgDate, diff)).toEqual(newDate);
+  });
+
+  it('returns a new date with 13 minutes added to the original', () => {
+    const orgDate = new Date('December 25, 1994 03:24:00');
+    const diff = '13m';
+    const newDate = new Date('December 25, 1994 03:37:00');
+    expect(add(orgDate, diff)).toEqual(newDate);
+  });
+
+  it('returns a new date with 100 seconds added to the original', () => {
+    const orgDate = new Date('January 15, 1990 05:00:00');
+    const diff = '100s';
+    const newDate = new Date('January 15, 1990 05:01:40');
+    expect(add(orgDate, diff)).toEqual(newDate);
+  });
+
 });
